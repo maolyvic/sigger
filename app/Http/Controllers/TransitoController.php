81af6581ac6405d6e701e1d41c58bf124e4af4b0
Ricardo <ricardoxgonzalez7427809@gmail.com>
2025-06-09 11:52:41 -0400

    Feat: Transito, integracion del Index

--- app/Http/Controllers/TransitoController.php
+++ app/Http/Controllers/TransitoController.php
@@ -3,13 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CausaMuerte; // Importar el modelo
 
 class TransitoController extends Controller
 {
     public function index()
     {
-        
-        return view('transito.index');
+        $transito = CausaMuerte::select(
+            'numero_entrada',
+            'despacho',
+            'numero_expediente',
+            'direccion_remocion_cadaver',
+            'delito',
+            'causa_muerte',
+            'mecanismo_causa_muerte',
+            'tipo_vehiculo',
+            'fecha_suceso_transito',
+            'fecha_ingreso_cadaver',
+            'hora',
+            'estado',
+            'municipio',
+            'parroquia',
+            'sector',
+            'direccion_exacta',
+            'categorizacion_referencias',
+            'nombres_apellidos',
+            'sexo',
+            'embarazada',
+            'edad',
+            'edad_medida',
+            'grupo_etario',
+            'tipo_identificacion',
+            'nacionalidad',
+            'identificacion',
+            'nivel_instruccion',
+            'sitio_donde_laboraba',
+            'descripcion',
+            'fecha_dictamen_muerte',
+            'hora_dictamen_muerte',
+            'fases_descomposicion',
+            'observaciones'
+        )->get();
+        return view('transito.index' , compact('transito'));
     }
 
     /**
@@ -25,7 +60,45 @@ class TransitoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'numero_entrada' => 'required|string',
+            'despacho' => 'required|string',
+            'numero_expediente' => 'required|string',
+            'direccion_remocion_cadaver' => 'required|string',
+            'delito' => 'required|string',
+            'causa_muerte' => 'required|string',
+            'mecanismo_causa_muerte' => 'required|string',
+            'tipo_vehiculo' => 'required|string',
+            'fecha_suceso_transito' => 'required|date',
+            'fecha_ingreso_cadaver' => 'required|date',
+            'hora' => 'required|string',
+            'estado' => 'required|string',
+            'municipio' => 'required|string',
+            'parroquia' => 'required|string',
+            'sector' => 'required|string',
+            'direccion_exacta' => 'required|string',
+            'categorizacion_referencias' => 'required|string',
+            'nombres_apellidos' => 'required|string',
+            'sexo' => 'required|string',
+            'embarazada' => 'nullable|boolean',
+            'edad' => 'required|integer',
+            'edad_medida' => 'required|string',
+            'grupo_etario' => 'required|string',
+            'tipo_identificacion' => 'required|string',
+            'nacionalidad' => 'required|string',
+            'identificacion' => 'required|string',
+            'nivel_instruccion' => 'required|string',
+            'sitio_donde_laboraba' => 'nullable|string',
+            'descripcion' => 'nullable|string',
+            'fecha_dictamen_muerte' => 'required|date',
+            'hora_dictamen_muerte' => 'required|string',
+            'fases_descomposicion' => 'nullable|string',
+            'observaciones' => 'nullable|string',
+        ]);
+
+        Transito::create($validatedData);
+
+        return redirect()->route('transito.index')->with('success', 'Registro creado exitosamente.');
     }
 
     /**
--- resources/views/transito/index.blade.php
+++ resources/views/transito/index.blade.php
@@ -1,20 +1,94 @@
+@php
+    $heads = [
+        'Número de Entrada',
+        'Despacho',
+        'Número de Expediente',
+        'Delito',
+        'Causa de Muerte',
+        'Tipo de Vehículo',
+        'Fecha Suceso Tránsito',
+        'Fecha Ingreso Cadáver',
+        'Hora',
+        'Dirección Exacta',
+        'Identificación',
+        'Acciones'
+    ];
+
+    $btnEdit = '<button class="btn btn-xs btn-default text-teal mx-1 shadow" title="Editar">
+    <i class="fa fa-lg fa-fw fa-pen"></i>
+</button>';
+    $btnDelete = '<button type="submit" class="btn btn-xs btn-default text-danger mx-1 shadow" title="Eliminar">
+    <i class="fa fa-lg fa-fw fa-trash-alt"></i>
+</button>';
+    $btnDetails = '<button class="btn btn-xs btn-default text-teal mx-1 shadow" title="Ver Detalle">
+    <i class="fa fa-lg fa-fw fa-eye"></i>
+</button>';
+
+    $config = [
+        'order' => [[0, 'asc']],
+        'columns' => [ null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        'language' => [
+            'url' => '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json',
+        ],
+    ];
+@endphp
+
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Tránsito')
 
 @section('content_header')
-    <h1>Lista de transito</h1>
+    <h1>{{ __('Datos de Tránsito') }}</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
-
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
-
-@section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <div class="main">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <a href="{{ route('transito.create') }}" class="btn bg-success float-right"><i class="fas fa-plus"></i>
+                            Nuevo</a>
+                    </div>
+                    <div class="card-body">
+                        <x-adminlte-datatable id="table-transito" :heads="$heads" :config="$config" striped hoverable without-buttons>
+                            @if (count($transito) > 0)
+                                @foreach ($transito as $item)
+                                    <tr>
+                                        <td>{{ $item->numero_entrada }}</td>
+                                        <td>{{ $item->despacho }}</td>
+                                        <td>{{ $item->numero_expediente }}</td>
+                                        <td>{{ $item->delito }}</td>
+                                        <td>{{ $item->causa_muerte }}</td>
+                                        <td>{{ $item->tipo_vehiculo }}</td>
+                                        <td>{{ $item->fecha_suceso_transito }}</td>
+                                        <td>{{ $item->fecha_ingreso_cadaver }}</td>
+                                        <td>{{ $item->hora }}</td>
+                                        <td>{{ $item->direccion_exacta }}</td>
+                                        <td>{{ $item->identificacion }}</td>
+                                        <td>
+                                            <div class="btn-group">
+                                                <a class="formVer" style="display: inline-block; margin-right: 5px;" href="{{ route('transito.show', $item->id) }}">
+                                                    {!! $btnDetails !!}
+                                                </a>
+                                                <form class="formEliminar" style="display: inline;" action="{{ route('transito.destroy', $item->id) }}" method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    {!! $btnDelete !!}
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td colspan="25" class="text-center">No se encontraron registros</td>
+                                </tr>
+                            @endif
+                        </x-adminlte-datatable>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
