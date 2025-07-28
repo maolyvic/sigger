4bd72b2a108fa44335916e5172f07e4ed5c30036
Ricardo <ricardoxgonzalez7427809@gmail.com>
2025-07-28 15:44:45 -0400

    DB: Agregado los sectores de AM,AN,AP y AR

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             RedipSeeder::class,
             EstadoSeeder::class,
             MunicipioSeeder::class,
+            ParroquiaSeeder::class,
         ]);
     }
 }
